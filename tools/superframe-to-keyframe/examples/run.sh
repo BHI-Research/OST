3c68aa10e96be8caf3a9b321ff43c4d808c6d9f8
Leandro Balmaceda <balmacedalm@gmail.com>
2017-10-21 16:57:37 -0300

    Add superframe to keyframe converter examples

---
+++ tools/superframe-to-keyframe/examples/run.sh
@@ -0,0 +1,4 @@
+python ../superframe-to-keyframe.py --input input.csv --output output-first.csv --frame first
+python ../superframe-to-keyframe.py --input input.csv --output output-last.csv --frame last
+python ../superframe-to-keyframe.py --input input.csv --output output-middle.csv --frame middle
+python ../superframe-to-keyframe.py --input input.csv --output output-random.csv --frame random
