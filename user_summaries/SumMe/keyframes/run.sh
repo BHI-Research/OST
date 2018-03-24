781a2b407b6c242a6b8e225478fe6e859dc62b2f
Leandro Balmaceda <balmacedalm@gmail.com>
2018-03-24 15:25:51 -0300

    Update structure of the User Summaries folder

--- user_summaries/SumMe/keyframes/run.sh
+++
@@ -1,6 +0,0 @@
-python ../superframe-to-keyframe.py --input input.csv --output first-v1.csv --frame first
-python ../superframe-to-keyframe.py --input input.csv --output last-v1.csv --frame last
-python ../superframe-to-keyframe.py --input input.csv --output middle-v1.csv --frame middle
-python ../superframe-to-keyframe.py --input input.csv --output random-v1.csv --frame random
-
-python ../../../tools/superframe-to-keyframe/superframe-to-keyframe.py  --input ../superframes/v1.csv --output first/v1.csv --frame first
